0bb4bff64123f6b6624d6379a15c16f980937d18
bengbmccoy <benjamingbmccoy@gmail.com>
2020-07-22 10:03:03 +1000

    First commit

---
+++ client.py
@@ -0,0 +1,14 @@
+'''
+Written By: Ben McCoy, July 2020
+Python 3.7.3
+
+client.py
+'''
+
+
+
+def main():
+    print('Suh Dudes')
+
+if __name__ == "__main__":
+    main()
---
+++ server.py
@@ -0,0 +1,13 @@
+'''
+Written By: Ben McCoy, July 2020
+Python 3.7.3
+
+server.py
+'''
+
+
+def main():
+    print('Suh Dudes')
+
+if __name__ == "__main__":
+    main()
